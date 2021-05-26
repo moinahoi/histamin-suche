e97a9513419d1148c9050b2aaa5222230c216196
moinahoi <post2710@gmail.com>
2021-05-26 07:32:51 +0200

    first commit

---
+++ public/embed/README.md
@@ -0,0 +1,7 @@
+# Embedded Histamin-Suche
+
+This site is to demonstrate how to embed the Histamin-Suche website in your site. It shows how it performs in a small iframe and which settings you can use to optimize the user experience.
+
+# Font
+
+The font used for the wireframe texts is called BLOKK ([GitHub](https://github.com/los-gordos/BLOKK)).
---
+++ public/sw.js
@@ -0,0 +1,41 @@
+const cacheOnInstall = ["./", "index.html", "icon/favicon-96x96.png"];
+const cacheVersion = "1.0";
+const cacheName = "Histamin-Suche";
+
+// pre-cache PWA on install
+self.addEventListener("install", (e) => {
+  // self.skipWaiting();
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(cacheOnInstall);
+    })
+  );
+});
+
+// listen to fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    // cache first, then network with caching
+    caches.match(event.request, { ignoreSearch: true }).then((response) => {
+      if (response) {
+        return response;
+      }
+      return (
+        // fetch ressource
+        fetch(event.request).then((response) => {
+          // cache fetched ressource clone
+          return caches.open(cacheName).then((cache) => {
+            cache.put(event.request.url, response.clone());
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener("message", function (event) {
+  if (event.data.action === "skipWaiting") {
+    self.skipWaiting();
+  }
+});
