2de61da8433fca56ee07a2449446d58dd09d95e9
moinahoi <post2710@gmail.com>
2021-06-29 07:11:44 +0200

    fix ios not displaying box-shadow correctly

--- app/sw.js
+++ app/sw.js
@@ -1,5 +1,5 @@
 const cacheOnInstall = ["./", "index.html", "icon/favicon-96x96.png"];
-const cacheVersion = "1.2.2";
+const cacheVersion = "1.3";
 const cacheName = "Histamin-Suche";
 
 // pre-cache PWA on install
